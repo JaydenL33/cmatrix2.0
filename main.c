6e1e19667aa585172f67d2245b3e881e01db7a52
Sebastian Southern <sebastian.southern@gmail.com>
2019-05-09 19:43:35 +1000

    created basic app

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+    Authors:    
+   - Sebastian Southern 
+   - Ben Gillespie
+   - Lachlan Leslie
+   - Albert Ferguson
+   - Jayden Lee
+   - Hong Kung (steve)
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void print_menu();
+void print_raindrops(char *input);
+void compress();
+int encrypt();
+int decrypt();
+int save();
+int load();
+void pepe();
+void squash_pepe();
+
+int main(void) {
+
+    return 1;
+}
