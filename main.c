45be0fa7243b44400550c848e1c76b239c81e65a
theQuantumMechanic <albertferguson118@gmail.com>
2019-05-10 15:40:13 +1000

    Update function prototypes
    Added encryption/decryption prototypes and fixed/added comments.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /*
-    Authors:    
-   - Sebastian Southern 
-   - Ben Gillespie
-   - Lachlan Leslie
-   - Albert Ferguson
-   - Jayden Lee
-   - Hong Kung (steve)
-*/
+ *   Authors:    
+ *  - Sebastian Southern 
+ *  - Ben Gillespie
+ *  - Lachlan Leslie
+ *  - Albert Ferguson
+ *  - Jayden Lee
+ *  - Hong Kung (Steven)
+ */
 
 
 #include <stdio.h>
@@ -14,15 +14,32 @@
 #include <string.h>
 #include <math.h>
 
+/* app functionality */
 void print_menu();
-void print_raindrops(char *input);
+void print_raindrops(char* input);
+
+/* Compression functionality */
 void compress();
-int encrypt();
-int decrypt();
-int save();
-int load();
+void decompress();
+
+/* Encryption/Decryption functionality */
+int xor_encrypt(char* plaintext, char* ciphertext, char* keystream);
+/* Note: decryption is simply reapplying xor_encrypt with the keystream */
+int decrypt(char* plaintext, char* ciphertext, char* keystream);
+/* Generators for stream cipher algorithm */
+int gen_key(void);
+int gen_pseudo_rand_key(int* key);
+/* state vector initiliser - 256 list based on key */
+int initialisation(int* key);
+
+/* file handling */
+int save(char* input);
+int load(char* output);
+
+/* memes be like... */
 void pepe();
 void squash_pepe();
+void matrix_quotes();
 
 int main(void) {
 
