4e626d11de403940530cf6364932f30a34ef9698
rlcaust <Jayden@rlc.com.au>
2019-05-30 14:18:33 +1000

    Update test.c

--- test.c
+++ test.c
@@ -4,12 +4,11 @@
 
 /* Function Prototype */
 
-int getArguments(int arguCounter,char* argv[]);
+int getArguments(int arguCounter, char* argv[]);
 
 
 int main(int argc, char *argv[])
 {
-
     getArguments(argc, argv);
     return 1; 
 }
@@ -17,7 +16,6 @@ int main(int argc, char *argv[])
 int getArguments(int arguCounter, char* argv[]) 
 { 
     int i; 
-
     printf("Program Name Is: %s",argv[0]); 
     if(arguCounter==1) 
     {
@@ -26,25 +24,25 @@ int getArguments(int arguCounter, char* argv[])
     }
 
     if(arguCounter>=2) 
-    { 
+    {
         printf("\nNumber Of Arguments Passed: %d",arguCounter); 
         printf("\n Case and Switch Testing "); 
-        for (i=0; i<arguCounter - 1; i++) 
+        for (i=0; i<arguCounter; i++) 
         {
             if (strcmp(argv[i], "-c") == 0)
             {
                 printf("\n The Code is %s, and the number in front of it is %s. \n", argv[i], argv[i+1]);
-
                 if (strcmp(argv[i + 1], "1") == 0)
                 {
                     printf("HOLY FUCK ITS WORK \n");
+                    
                 }
             }
             if (strcmp(argv[i], "-E") == 0)
             {
-                i = arguCounter;
-            }
-        }      
+                
+            }  
+        }
     } 
-    return caseOutPut; 
+    return 0; 
 } 
