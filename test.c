4018db410e77dce35ec9be12328f5ff8c988c132
rlcaust <Jayden@rlc.com.au>
2019-05-31 19:22:31 +1000

    Fixed Printing Error
    Error in Printing Fixed.

--- test.c
+++ test.c
@@ -10,48 +10,41 @@
  */
 
 
-int getArguments(int arguCounter, char* argv[]);
-
+int checkValidRange (unsigned char *unCheckedArray, int plaintextlen, char* checkedArray);
 
 int main(int argc, char *argv[])
 {
-    getArguments(argc, argv);
-    return 1; 
-}
-  
-int getArguments(int arguCounter, char* argv[]) 
-{ 
-    int i; 
-    printf("Program Name Is: %s",argv[0]); 
-    if(arguCounter==1) 
-    {
-        printf("Test \n");
-        /* Execute Some Function */
-    }
+    int amount = 10;
+    unsigned char testArray = "test";
+    printf("test array is %s", testArray);
+    char CheckedArray[amount];
+    checkValidRange(testArray, amount, CheckedArray);
+    printf("Checked Range is %s", CheckedArray);
 
-    if(arguCounter>=2) 
+}
+
+/**************************************************************************
+ * We need to get this to work in main.c. 
+ * Take in options like colour and set the value of color to ?
+ * print list if invalid option. 
+ * crappy input, no input. 
+****************************************************************************/
+  
+int checkValidRange (unsigned char *unCheckedArray, int plaintextlen, char* checkedArray) {
+    int decASCII;
+    int i;
+    int j = 0;
+    
+    for (i = 0; i < plaintextlen; i++)
     {
-        printf("\nNumber Of Arguments Passed: %d",arguCounter); 
-        printf("\n Case/if Statement Testing. "); 
-        for (i=0; i<arguCounter; i++) 
+        decASCII = (int) unCheckedArray[i]; 
+        if (decASCII >= 33 && decASCII <= 126)
         {
-            if ((int) *argv[i] == '-')
-            {
-                if (strcmp(argv[i], "-c") == 0)
-                {
-                    printf("\n The Code is %s, and the number in front of it is %s. \n", argv[i], argv[i+1]);
-                    if (strcmp(argv[i + 1], "1") == 0)
-                    {
-                        printf("HOLY FUCK ITS WORK \n");
-                        
-                    }
-                }
-                if (strcmp(argv[i], "-E") == 0)
-                {
-                    
-                }  
-            }
+            checkedArray[j] = (char) (decASCII - 127) ;
+            j++;
         }
-    } 
-    return 0; 
-} 
+    }
+    checkedArray[j] = '\0';
+    return 1;
+}
+
